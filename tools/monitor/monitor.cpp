34da6e6f7740d47dedd9156cbfbd861005f299b2
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-02-20 15:39:27 +0200

    contextkit-monitor: exit QCoreApplication on signal
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- tools/monitor/monitor.cpp
+++ tools/monitor/monitor.cpp
@@ -7,15 +7,37 @@
 #include <QRegExp>
 #include <map>
 #include <memory>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <QSocketNotifier>
+
+static int sigFd[2];
+
+void onExit(int)
+{
+    char a = 1;
+    ::write(sigFd[0], &a, sizeof(a));
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
+    QSocketNotifier *sigNot;
     auto args = app.arguments();
     if (args.size() <= 1) {
         qDebug() << "Usage: " << args[0] << " <namespace_path>...";
         return -1;
     }
+    ::socketpair(AF_UNIX, SOCK_STREAM, 0, sigFd);
+    sigNot = new QSocketNotifier(sigFd[1], QSocketNotifier::Read, &app);
+    app.connect(sigNot, &QSocketNotifier::activated, []() {
+            auto app = QCoreApplication::instance();
+            if (app)
+                app->quit();
+        });
+    for (auto i : {SIGTERM, SIGINT})
+        ::signal(i, onExit);
     auto dirname = args[1];
     QDir d(dirname);
     auto files = d.entryList(QDir::Files);
