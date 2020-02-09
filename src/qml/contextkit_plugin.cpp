0d6a51cb6894630ab399c6fd597b58e7bf76fc4b
Bea Lam <bea.lam@jollamobile.com>
2020-02-10 08:57:26 +1000

    [statefs-qt] Rename contextkit QML module and package. Contributes to JB#48573
    Package contextkit-declarative-qt5 renamed to
    contextkit-statefs-declarative-qt5.
    
    Module org.freedesktop.contextkit renamed to Nemo.StateFs.
    
    The org.freedesktop.contextkit package will now be provided elsewhere.

--- src/qml/contextkit_plugin.cpp
+++ src/qml/contextkit_plugin.cpp
@@ -1,8 +1,23 @@
-/**
- * @file client.hpp
- * @brief Statefs QML plugin
- * @copyright (C) 2013-2014 Jolla Ltd.
- * @par License: LGPL 2.1 http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+/*
+ * Copyright (c) 2013 - 2020 Jolla Ltd.
+ * Copyright (c) 2020 Open Mobile Platform LLC.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
  */
 
 #include "contextkit_plugin.hpp"
@@ -12,5 +27,6 @@
 
 void ContextkitPlugin::registerTypes(char const* uri)
 {
+    Q_ASSERT(QLatin1String(uri) == QLatin1String("Nemo.StateFs"));
     qmlRegisterType<ContextPropertyDeclarative>(uri, 1, 0, "ContextProperty");
 }
